f3467e14871a558bc92f9db743f72000c5a77818
Jeewan <jeewantharu234@gmail.com>
2025-02-06 12:44:39 +0530

    HomePAGE UPDATED

--- src/Pages/HomeSection/MainContent.jsx
+++ src/Pages/HomeSection/MainContent.jsx
@@ -1,11 +1,17 @@
-// src/components/HomeImage.jsx
-// import React from 'react';  // Correct import for React
-import "/src/Styles/MainContent.css";  // Import your CSS file
+import "/src/Styles/MainContent.css"; 
 
 const MainContent = () => {
   return (
     <div className="main-content">
       <img src="src/assets/homeimage.png" alt="Home" className="home-image" />
+      <div className="overlay-card">
+        <div className="card-text">
+        <h2>What can I make with.....</h2><br /><br />
+        <p>Our recipe finder tool will show you all the things you can make,<br />
+        So none of your food goes to waste, with only a few added <br />
+        ingredients needed.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import Navbar from '/src/Pages/Navbar'
-import ProfilePage from '/src/Pages/Profilesection/ProfilePage'
+import HomePage from './Pages/HomeSection/HomePage';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ProfilePage />
+    <HomePage/>
   </StrictMode>,
 )
