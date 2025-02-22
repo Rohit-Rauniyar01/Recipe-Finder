c2a604de9a0d9a3fa5096d51bf29e6f3130053d7
Jeewan <jeewantharu234@gmail.com>
2025-02-22 23:52:56 +0530

    Login and Signup page added

--- src/Pages/profilesection/Signup.jsx
+++ src/Pages/profilesection/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "/src/Styles/Signup.css";
+import "../../Styles/Signup.css";
 
 const Signup = () => {
   const [username, setUsername] = useState("");
@@ -22,12 +22,24 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSignup}>
-      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-      <button type="submit">Signup</button>
-    </form>
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSignup}>
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <label>Username</label>
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button className="signup-btn" type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 };
 
