3ecac62b80a16fb42908f52ac0e143796ed01bda
Rohit-Rauniyar01 <rohitgupta2019email@gmail.com>
2025-02-27 10:36:30 +0530

    Saved local changes before pulling feature-branch

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import Navbar from '/src/Pages/Navbar'
-import ProfilePage from '/src/Pages/Profilesection/ProfilePage'
+// import ProfilePage from '/src/Pages/Profilesection/ProfilePage'
+import HomePage from './Pages/HomePage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ProfilePage />
+    <HomePage />
   </StrictMode>,
 )
